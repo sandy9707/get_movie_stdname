5b5d454430488a4854fcb58872fc3e8c1b1c2392
sandy9707 <Sandy970701@Gmail.com>
2025-01-06 21:08:06 +0800

    2025-01-06 21-08-06

--- gitp_shell_floder.sh
+++ gitp_shell_floder.sh
@@ -1,10 +1,12 @@
 # #!/bin/zsh
 
-SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+SHELL_FOLDER=$(
+    cd "$(dirname "$0")"
+    pwd
+)
 echo $SHELL_FOLDER
 cd $SHELL_FOLDER
 git add .
-commitTime=`date +"%Y-%m-%d %H-%M-%S"`
+commitTime=$(date +"%Y-%m-%d %H-%M-%S")
 git commit -a -m "${commitTime}"
 git push origin main
-
